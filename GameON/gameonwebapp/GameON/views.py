7f531848b8b12fd6fb1430a9060f1bb2a8e8416f
Nishanthnishu <nishanthkwel@gmail.com>
2017-07-26 04:26:40 +0530

    yet to create signup

--- GameON/gameonwebapp/GameON/views.py
+++ GameON/gameonwebapp/GameON/views.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def signup(request):
+    #https://simpleisbetterthancomplex.com/tutorial/2017/02/18/how-to-create-user-sign-up-view.html
+    pass
